5359701b7d880c9e70813544a2f856c28a15d0ff
sourabh <sourabhprajapati759@gmail.com>
2025-07-31 07:15:17 -0700

    connected to db

---
+++ shopify/Backend/server.js
@@ -0,0 +1,23 @@
+const http=require('http');
+const app=require('./app');
+const port=process.env.PORT || 4000;
+const ConnectToDB=require('./db/db');
+
+const server=http.createServer(app);//create the server here 
+
+
+ConnectToDB();//connect database 
+
+
+server.listen(port,()=>{
+    console.log(`server is running at port  http://localhost:${port}}`);
+})
+
+
+
+
+
+
+
+
+ 
