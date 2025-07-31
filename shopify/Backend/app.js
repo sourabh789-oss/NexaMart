0838a09e12bd2f30af15a853721a2131d3801a99
sourabh <sourabhprajapati759@gmail.com>
2025-07-31 07:16:19 -0700

    updated

---
+++ shopify/Backend/app.js
@@ -0,0 +1,21 @@
+const dotenv=require('dotenv');
+dotenv.config();//for use the .env file key 
+const cookieParser = require('cookie-parser');
+const express=require('express');
+const app=express();
+const cors=require('cors');
+
+
+//for parsing the body request data when we give data on server 
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+app.get('/',(req,res)=>{
+     res.send("Hello World");
+})
+
+
+
+module.exports=app;
