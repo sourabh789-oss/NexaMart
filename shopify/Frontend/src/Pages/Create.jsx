1efbde917091e137c4658fc34cbdf4bb7422f86b
sourabh <sourabhprajapati759@gmail.com>
2025-08-06 13:31:56 -0700

    update styling

--- shopify/Frontend/src/Pages/Create.jsx
+++ shopify/Frontend/src/Pages/Create.jsx
@@ -4,12 +4,13 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 function Create() {
 
+   
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [no, setNo] = useState("");
     const [password, setPassword] = useState("");
 
-     
+
     const submitted = (e) => {
         e.preventDefault();
         setName("");
@@ -21,38 +22,38 @@ function Create() {
 
 
     return (<motion.div className='formcontainer relative dark:text-white   mt-[3%] left-[30%]' drag dragConstraints={{
-         left:0,
-          right:400,
-          bottom:0,
-          top:100
+        left: 0,
+        right: 400,
+        bottom: 0,
+        top: 100
     }}
 
-      animate={{
-         opacity:[0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.9,1]
-       }}
-    
-       transition={{
-         delay:0.1,
-         duration:1.4,
-          
-       }}
-       
-     whileDrag={{
-         scale:0.8
-     }}
+        animate={{
+            opacity: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.9, 1]
+        }}
 
-    
-       dragElastic={2}
+        transition={{
+            delay: 0.1,
+            duration: 1.4,
+
+        }}
+
+        whileDrag={{
+            scale: 0.8
+        }}
+
+
+        dragElastic={2}
         dragTransition={{
-             bounceStiffness:5,
-          
-           
+            bounceStiffness: 5,
+
+
         }}
     >
         <h1 className='text-4xl mb-4 font-mono  text-[#90e027] dark:text-red-700'>Create Your Account</h1>
         <form onSubmit={(e) => {
             submitted(e);
-        }} className='flex flex-col gap-4  w-1/3 border border-[#e5e7eb] rounded-md  bg-[#f9f9f9] dark:bg-black text-black dark:text-white  dark:border-gray-200 p-5'>
+        }} className='flex flex-col gap-4  w-1/3 border-2  border-white/20  rounded-md  bg-[#f9f9f9] backdrop-blur-md shadow-xl dark:bg-white/5  text-black dark:text-white  dark:border-white/10 p-5'>
             <input value={name} onChange={(e) => {
                 setName(e.target.value)
             }} type="text" name="yourname" className=' border outline-none focus:border-[1.9px]  focus:border-blue-500 h-9 placeholder:text-base  text-xl rounded-md' id="yourname" placeholder='Enter Your name' />
@@ -61,7 +62,7 @@ function Create() {
             }} type="email" name='youremail' className='border outline-none focus:border-[1.9px] focus:border-blue-500 placeholder:text-base  text-xl   h-9 rounded-md' id='youremail' placeholder='Enter Your email' />
             <input value={password} onChange={(e) => {
                 setPassword(e.target.value)
-            }} type="password" name='yourpassword' className=' border  focus:border-[1.9px] focus:border-blue-500  outline-none h-9 rounded-md placeholder:text-base  text-xl' id='yourpassword' placeholder='Enter Your password' />
+            }} type="password" name='yourpassword' className=' border  focus:border-[1.9px] focus:border-blue-500 focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50  outline-none h-9 rounded-md placeholder:text-base  text-xl' id='yourpassword' placeholder='Enter Your password' />
             <input value={no} onChange={(e) => {
                 setNo(e.target.value);
             }} type="number" name='yournumber' className='border  focus:border-[1.9px] focus:border-blue-500  outline-none h-9 rounded-md  placeholder:text-base  text-xl' id='yournumber' placeholder='Enter your number' />
