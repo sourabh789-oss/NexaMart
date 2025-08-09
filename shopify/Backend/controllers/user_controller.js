94e006832b528dac1f1fe04c3c3032f8d5ea2f39
sourabh <sourabhprajapati759@gmail.com>
2025-08-09 08:20:06 -0700

    register user functionality and hashed password

---
+++ shopify/Backend/controllers/user_controller.js
@@ -0,0 +1,48 @@
+
+// In this file  basically we work on create the user account  login account ,profile view 
+const userModel = require('../model/user.model');
+const userService = require('../services/user_service');
+const { validationResult } = require('express-validator');//for automatically detect when something is wrong in  my code then it automatic detect the error and perform the action on user data 
+
+module.exports.registerUser = async (req, res, next) => {
+
+    //check first error in the user request 
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+
+    //extract this value from the user data body 
+    const { fullname, Phoneno, email, password } = req.body;
+
+    const isUserAlreadyExist = await userModel.findOne({ email, Phoneno });//check if this both found in our database 
+
+    if (isUserAlreadyExist) {//means this user is found already 
+        return res.status(400).json({ message: 'User already exists' });
+
+    }
+
+
+    //hash the password 
+    const hashPassword = await userModel.hashPassword(password)
+
+    const user = await userService.CreateUserAccount({
+        firstname: fullname.firstname,
+        Lastname: fullname.Lastname,
+        Phoneno: Phoneno,
+        email,
+        password: hashPassword//when create the user Account stored password in encrypted form in our database 
+
+    })
+
+    //now create the token 
+    const token = user.generateAuthToken();
+
+    return res.status(201).json({ token, user });//after successfully create the account it return response token and user 
+
+}
+
+
+
