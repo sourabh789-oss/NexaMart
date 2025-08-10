423e94161349a7405a6992c517b189e36ab179b9
sourabh <sourabhprajapati759@gmail.com>
2025-08-10 13:27:43 -0700

    add login route

--- shopify/Backend/server.js
+++ shopify/Backend/server.js
@@ -1,7 +1,7 @@
 const http=require('http');
 const app=require('./app');
 const port=process.env.PORT || 4000;
-const ConnectToDB=require('./db/db');
+const ConnectToDB=require('./db/db.js');
 
 const server=http.createServer(app);//create the server here 
 
