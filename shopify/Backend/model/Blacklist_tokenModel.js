c04b924706c45b921321a1f0e2eda5533e727d7b
sourabh <sourabhprajapati759@gmail.com>
2025-08-11 06:00:18 -0700

    create blackList schema for authentication

---
+++ shopify/Backend/model/Blacklist_tokenModel.js
@@ -0,0 +1,26 @@
+const mongoose=require('mongoose');
+
+
+ const blackListTokenSchema= new mongoose.Schema({
+
+  token:{
+     type:String,
+     unique:true,
+      required:true 
+  },
+  createdAt:{
+     type:Date,
+     default:Date.now(),
+      expires:86400
+  }
+
+
+
+
+
+ });
+
+
+  const TokenModel=mongoose.model('BlacklistToken',blackListTokenSchema);
+
+   module.exports=TokenModel;
