94882958527a3bc4206f33fbe03273b07f6ecdbc
sourabh <sourabhprajapati759@gmail.com>
2025-08-13 13:55:47 -0700

    nothing too much

---
+++ shopify/Frontend/src/context/UserContext.jsx
@@ -0,0 +1,26 @@
+import React, { createContext, useState } from 'react'
+
+export const UserdataContext=createContext();
+
+const UserContext = ({children}) => {
+
+    //create a state that contains user data which is empty and set when we create the account 
+ const [userdata,setuserdata]=useState({
+    fullname:{
+         firstname:"",
+         Lastname:""
+    },
+    email:"",
+    
+ })
+
+  return (
+    <div>
+         <UserdataContext.Provider value={{userdata ,setuserdata}}>
+             {children}
+         </UserdataContext.Provider>
+        </div>
+  )
+}
+
+export default UserContext
