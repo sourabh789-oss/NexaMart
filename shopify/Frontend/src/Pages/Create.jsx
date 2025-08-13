aebf5661d2920461a314654ce105a838ffe76c6d
sourabh <sourabhprajapati759@gmail.com>
2025-08-13 13:51:31 -0700

    integrate frontend to backend

--- shopify/Frontend/src/Pages/Create.jsx
+++ shopify/Frontend/src/Pages/Create.jsx
@@ -1,19 +1,66 @@
 import { motion } from 'motion/react';
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { UserdataContext } from '../context/UserContext';
+import { Link, useNavigate } from 'react-router-dom'
+import {ProfileData} from '../context/ProfileContext'
+import axios from 'axios';
 
-import { Link } from 'react-router-dom'
 function Create() {
+const navigate=useNavigate();
 
-   
-    const [name, setName] = useState("");
+    const [Firstname, setFirstname] = useState("");
+    const [Lastname, setLastname] = useState("");
     const [email, setEmail] = useState("");
     const [no, setNo] = useState("");
     const [password, setPassword] = useState("");
 
 
-    const submitted = (e) => {
+
+    const { userdata, setuserdata } = useContext(UserdataContext);//extact our provider value from userContext userdata,setuserdata
+
+  const {setAccountcreate,setLoggedIn,setfirstname,setlastname,setemail}=useContext(ProfileData);
+
+
+    
+    const submitted = async (e) => {
         e.preventDefault();
-        setName("");
+          const UserData={
+              fullname:{
+                  firstname:Firstname,
+                  Lastname:Lastname 
+                },
+                email:email,
+                Phoneno:no,
+                password:password 
+                
+          }
+
+          setfirstname(Firstname),setlastname(Lastname),setemail(email)
+
+          try{
+           const response =await axios.post(`${import.meta.env.VITE_BASE_URL}/user/Register`,UserData);
+               if(response.status===201){//means everything is ok 
+                   const data=response.data;
+                     
+                   //set the data in context api and token in our local storage 
+                    setuserdata(data.user);
+                     localStorage.setItem("token",data.token)
+                    setAccountcreate(true); 
+                     setLoggedIn(true);
+                     navigate('/');
+                     
+               }
+
+           }catch(error){
+             console.log(error);
+           }
+            
+
+        // post request api call on backend with data UserData;
+
+
+        setFirstname("");
+        setLastname("");
         setEmail("");
         setNo("");
         setPassword("");
@@ -21,7 +68,7 @@ function Create() {
     }
 
 
-    return (<motion.div className='formcontainer relative dark:text-white   mt-[3%] left-[30%]' drag dragConstraints={{
+    return (<motion.div className='formcontainer box-border relative dark:text-white   mt-[3%] left-[30%]' drag dragConstraints={{
         left: 0,
         right: 400,
         bottom: 0,
@@ -54,19 +101,31 @@ function Create() {
         <form onSubmit={(e) => {
             submitted(e);
         }} className='flex flex-col gap-4  w-1/3 border-2  border-white/20  rounded-md  bg-[#f9f9f9] backdrop-blur-md shadow-xl dark:bg-white/5  text-black dark:text-white  dark:border-white/10 p-5'>
-            <input value={name} onChange={(e) => {
-                setName(e.target.value)
-            }} type="text" name="yourname" className=' border outline-none focus:border-[1.9px]  focus:border-blue-500 h-9 placeholder:text-base  text-xl rounded-md' id="yourname" placeholder='Enter Your name' />
+            <div className='flex gap-3' >
+                <input value={Firstname} onChange={(e) => {
+                    setFirstname(e.target.value)
+                }} type="text"  className=' border pl-2 w-3/4 outline-none focus:border-[1.9px]  focus:border-blue-500 h-9 placeholder:text-base  text-xl rounded-md' placeholder='Firstname' />
+
+                <input value={Lastname} onChange={(e) => {
+                    setLastname(e.target.value)
+                }} type="text"  className='w-3/4 pl-2 border outline-none focus:border-[1.9px]  focus:border-blue-500 h-9 placeholder:text-base  text-xl rounded-md' placeholder='Lastname' />
+
+            </div>
+
             <input value={email} onChange={(e) => {
                 setEmail(e.target.value)
             }} type="email" name='youremail' className='border outline-none focus:border-[1.9px] focus:border-blue-500 placeholder:text-base  text-xl   h-9 rounded-md' id='youremail' placeholder='Enter Your email' />
+
             <input value={password} onChange={(e) => {
                 setPassword(e.target.value)
             }} type="password" name='yourpassword' className=' border  focus:border-[1.9px] focus:border-blue-500 focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50  outline-none h-9 rounded-md placeholder:text-base  text-xl' id='yourpassword' placeholder='Enter Your password' />
+
             <input value={no} onChange={(e) => {
                 setNo(e.target.value);
             }} type="number" name='yournumber' className='border  focus:border-[1.9px] focus:border-blue-500  outline-none h-9 rounded-md  placeholder:text-base  text-xl' id='yournumber' placeholder='Enter your number' />
+
             <button className=' px-4 py-2 font-bold  bg-[#27E0B3]'>Create Account</button>
+
             <span className='text-center text-gray-400  dark:text-white'>Already have an account? <Link className=' underline decoration-blue-600 text-blue-600' to={'/Login'}>Login here</Link></span>
         </form>
 
