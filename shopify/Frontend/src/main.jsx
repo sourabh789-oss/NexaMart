34e3c52ee54ad5744003e75ca24570efa295dca3
sourabh <sourabhprajapati759@gmail.com>
2025-08-13 13:55:21 -0700

    give profilecontext wrapper

--- shopify/Frontend/src/main.jsx
+++ shopify/Frontend/src/main.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx'
 import './index.css'
-
+import UserContext from "./context/UserContext.jsx";
+import ProfileContext from "./context/ProfileContext.jsx";
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <ProfileContext>
+        <UserContext>
+          <App />
+        </UserContext>
+      </ProfileContext>
+
+    </BrowserRouter>
+  </StrictMode>
 )
