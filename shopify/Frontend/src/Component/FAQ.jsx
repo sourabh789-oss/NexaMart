16ad41585ef42f6184b0eea5e4ee8eb45e55b2e3
sourabh <sourabhprajapati759@gmail.com>
2025-08-18 13:41:25 -0700

    nothing too

--- shopify/Frontend/src/Component/FAQ.jsx
+++ shopify/Frontend/src/Component/FAQ.jsx
@@ -62,7 +62,7 @@ const FAQ = () => {
         {
           data.map((element, idx) => {
             return <div key={idx} className='mx-52 mt-1 flex flex-col gap-1 w-2/3'>
-              <div onClick={() => toggleAnswer(idx)} className=' cursor-pointer flex items-center dark:bg-gray-800  dark:hover:bg-[#414141]  dark:text-[#ffff] justify-between px-7 py-4 '>
+              <div onClick={() => toggleAnswer(idx)} className=' cursor-pointer flex items-center dark:bg-gray-800  dark:hover:bg-[#414141] bg-[#EEEFF0] hover:bg-[#d1d2d3] dark:text-[#ffff] justify-between px-7 py-4 '>
                 <h1 className='text-2xl '>{element.question}</h1>
                 <i className={`text-3xl  ${openStates[idx] ? "ri-close-large-line" : "ri-add-large-line"
                   }`}></i>
@@ -74,7 +74,7 @@ const FAQ = () => {
                   animate={{ opacity: 1, height: "auto" }}//when expand our height 
                   exit={{ opacity: 0, height: 0 }}//when again remove the height set to initial 
                   transition={{ duration: 0.35, ease: "easeInOut" }}
-                  className=" dark:bg-gray-800 text-start px-6  ">
+                  className=" dark:bg-gray-800 bg-[#EEEFF0] text-start px-6  ">
                   <p className='text-[1.3rem] px-2 py-4'> {element.answer} </p>
                 </motion.div>)}
               </AnimatePresence>
