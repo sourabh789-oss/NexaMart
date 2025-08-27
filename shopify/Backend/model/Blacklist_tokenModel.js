44a34d0f037d0b67503561b1954ddc213e79a948
sourabh <sourabhprajapati759@gmail.com>
2025-08-27 07:41:40 -0700

    increase expire time token

--- shopify/Backend/model/Blacklist_tokenModel.js
+++ shopify/Backend/model/Blacklist_tokenModel.js
@@ -1,26 +1,26 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
 
- const blackListTokenSchema= new mongoose.Schema({
+const blackListTokenSchema = new mongoose.Schema({
 
-  token:{
-     type:String,
-     unique:true,
-      required:true 
-  },
-  createdAt:{
-     type:Date,
-     default:Date.now(),
-      expires:86400
-  }
-
-
+    token: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 2592000
+    }
 
 
 
- });
 
 
-  const TokenModel=mongoose.model('BlacklistToken',blackListTokenSchema);
+});
 
-   module.exports=TokenModel;
+
+const TokenModel = mongoose.model('BlacklistToken', blackListTokenSchema);
+
+module.exports = TokenModel;
