201c8d2c321abd0bbe2b0da32fc182a2c1a3a1b4
sourabh <sourabhprajapati759@gmail.com>
2025-08-28 02:34:26 -0700

    solve cors issue on deployment

--- shopify/Frontend/vite.config.js
+++ shopify/Frontend/vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
- import  path from 'path'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //for connection on deployment 
+  server: {
+    proxy: {
+      '/user': {
+        target: process.env.VITE_BASE_URL,
+        changeOrigin: true,
+        secure: true
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
