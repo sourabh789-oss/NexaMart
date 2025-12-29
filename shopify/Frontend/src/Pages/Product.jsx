903bafc5a6c9287089ab34d5b75708814ef96a80
sourabh <sourabhprajapati759@gmail.com>
2025-12-29 20:41:16 +0530

    add tanstack query for fetching product data

--- shopify/Frontend/src/Pages/Product.jsx
+++ shopify/Frontend/src/Pages/Product.jsx
@@ -2,50 +2,40 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import Loader from '../Component/Loader';
 // import useLocoScroll from '../Scrollbehaviour/useLocoScroll';
-
+import { useQuery } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 
+
+const fetchProducts=async()=>{
+     const res=await axios.get(`${import.meta.env.VITE_PRODUCTS_DATA}`);
+     return res.data;
+  }
 
 
 function Product() {
 
   const navigate = useNavigate();
-  const [data, setData] = useState([]);
-  const [loader, setLoader] = useState(true);//initially our loader state true set 
-
-  // const scrollRef = useLocoScroll(true);
-  const getdata = async () => {
-    try {
-      const Response = await axios.get(`${import.meta.env.VITE_PRODUCTS_DATA}`);
-      // console.log(Response.data);
-      setData(Response.data);
-
-      setTimeout(() => {
-
-        setLoader(false);//jaise hi data fetch hua api se or data ko set krdiye then loader ki value false krdo jisse ab loader show nahi hoga 
-      }, 1500) //show the loader even data fetches from api for 1.5 second then disappear 
-    }
-    catch (error) {
-      console.error("API ERROR:", error);
-    }
-  }
-
-  useEffect(() => {
-    getdata();
+  
 
 
-  }, [])
+  
+
+const {
+  data,
+  isLoading,
+  isError,
+  error,
+}=useQuery({
+queryKey:['products'],
+queryFn:fetchProducts,
+staleTime:1000*60*5,//5 minutes cache means no need to fetch this data until more than 5 minutes 
+});
 
 
 
 
-  // useEffect(() => {
-  //   if (!loader && scrollRef.current) {
-  //     setTimeout(() => {
-  //       scrollRef.current.update();
-  //     }, 100)
-  //   }
-  // }, [loader])
+
+  
 
   //issue in this component :- scrollbar nahi show ho raha ha scroll hone par and proper scroll nahi ho raha ha 
 
@@ -59,18 +49,20 @@ function Product() {
 
 
 
-
+ if(isError){
+   return <p className='text-red-500'>{error.message}</p>
+ }
 
 
   // task give loader effect when we fetch the api syntax{ condition?(if true):(if false) }
   return (
-    <div className='min-h-screen '> {loader ? (
+    <div className='min-h-screen '> {isLoading? (
       <Loader />
     ) : (<>
       <h1 className='login text-4xl text-center pb-3  font-serif text-[#27E0B3]'>Our Products</h1>
       <div className="Products  text-black   flex flex-row justify-center items-center bg-[#FFFFFD] flex-wrap gap-4 w-full py-3 data-scroll-section ">
         {
-          data.map((element, idx) => {
+          data?.map((element, idx) => {
             return <div key={idx} className=' px-7 py-4 flex flex-col items-center gap-4 rounded-xl border border-gray-300 border-solid  font-mono  box-border '>
               <div className='bg-[#f6f6f6] rounded-md w-[15rem] ' >
                 <img className=' w-[15rem] h-[15rem] object-contain transition-all cursor-pointer hover:scale-125' src={element.image} alt="" />
